a85d6fcd6afddc56b627b68225525228c7041670
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-11 15:50:31 +0200

    Aggiunta commento e completamento esercizio.

--- script.js
+++ script.js
@@ -3,24 +3,33 @@
 // - Restituisce il testo con xxx al posto delle parole censurate.
 // - Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
 
-var elemento = prompt("Inserisci il testo da visualizzare");
+var elemento = document.getElementById('mio_id');
+var elemento1 = document.getElementById("mio_id1");
+var testo = prompt("Inserisci il testo da visualizzare");
+
+// metto il testo tutto a lettere grandi
+var paroleUppercase = testo.toUpperCase();
 // creo array per separare le parole
-var righe = elemento.split(" ");
+var righe = paroleUppercase.split(" ");
 // inizializzo a 0 variabile parole censurate
 var paroleCensurate = 0;
 // definisco il numero delle parole
 var numeroParole = righe.length;
 
+// array con parole proibite
+ var parole = ["CIAO", "CASA", "VAFFANCULO", "STRONZO", "CELLULARE", "STUPIDO", "DEFICIENTE", "SCUOLA"];
 
- var parole = ["ciao", "casa", "vaffanculo", "stronzo", "cellulare", "stupido", "deficiente"];
-
+// ciclo per verifica presenza parole proibite
 for (var i = 0; i < parole.length; i++) {
-   if (elemento.includes(parole[i])) {
-     elemento = elemento.replace(parole[i],"xxx");
+   if (paroleUppercase.includes(parole[i])) {
+     paroleUppercase = paroleUppercase.replace(parole[i],"xxx");
      paroleCensurate += 1;
    }
+}
 
- }
+// rimetto il testo tutto in lettere piccole
+var paroleLower = paroleUppercase.toLowerCase();
 
- console.log(elemento);
- console.log(paroleCensurate + "  parole censurate su " + numeroParole + " totali. ");
+// stampo le varie rischieste dell'esercizio
+ elemento.innerHTML = paroleLower;
+ elemento1.innerHTML = paroleCensurate + "  parole censurate su " + numeroParole + " totali. ";
